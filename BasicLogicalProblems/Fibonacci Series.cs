1233a889d514280e25f98339aaeca22b99af5a54
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-08 20:57:44 +0530

    [Mahesh]Add: Fibonacci Series

---
+++ BasicLogicalProblems/Fibonacci Series.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProblems
+{
+    internal class Fibonacci_Series
+    {
+        int n1 = 0, n2 = 1, n3, i;
+
+        public void Fibb()
+        {
+            Console.WriteLine("Enetr the number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Print First numbers "+"\n"+ n1 +"\n"+"second number"+"\n"+ n2 );
+            Console.WriteLine("and remaining numbers are: ");
+            for (i=2;i<number;i++)
+            {
+                n1 = n2;
+                n2 = n3;
+                n3 = n1 + n3;
+                Console.WriteLine(n3);
+            }
+
+        }
+    }
+}
--- BasicLogicalProblems/Program.cs
+++ BasicLogicalProblems/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+namespace BasicLogicalProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problems ");
+            Console.WriteLine("select options "+"\n"+"1.Fibonacci_Series:");
+            while (true)
+            { 
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Fibonacci_Series fibb = new Fibonacci_Series();
+                        fibb.Fibb();
+                        break;
+                    default:
+                        Console.WriteLine("please try again........");
+                        break;
+                }
+
+            }
+        }
+    }
+}
