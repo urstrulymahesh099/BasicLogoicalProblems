4222949147cf12cb2296f5894604bbc8a5779248
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-08 21:20:58 +0530

    [Mahesh]Add: Perfect number

---
+++ BasicLogicalProblems/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public void Perfect()
+        {
+            int number, sum = 0, n;
+            Console.Write("enter the Number");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- BasicLogicalProblems/Program.cs
+++ BasicLogicalProblems/Program.cs
@@ -7,15 +7,19 @@ namespace BasicLogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Problems ");
-            Console.WriteLine("select options "+"\n"+"1.Fibonacci_Series:");
+            Console.WriteLine("select options "+"\n"+"1.Fibonacci_Series:"+"\n"+ "2.PerfectNumber :");
             while (true)
             { 
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch(option)
-                {
+                { 
                     case 1:
                         Fibonacci_Series fibb = new Fibonacci_Series();
                         fibb.Fibb();
+                        break;
+                    case 2:
+                        PerfectNumber perfect = new PerfectNumber();
+                        perfect.Perfect();
                         break;
                     default:
                         Console.WriteLine("please try again........");
